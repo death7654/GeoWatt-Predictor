7c6f545e3033370882f45f5468ed03c1a9cdc772
Evan Iwin Biju <evaniwinbiju@gmail.com>
2025-12-02 22:55:02 +0530

    only testing remains

--- tools/training_data_fetcher/fetcher.py
+++ tools/training_data_fetcher/fetcher.py
@@ -2,6 +2,7 @@
 
 import requests
 import os
+import openpyxl
 from concurrent.futures import ThreadPoolExecutor
 
 # configuration
@@ -10,8 +11,8 @@ apikey = ""
 zoomlevel = 21
 imagewidth = 1000
 imageheight = 1000
-dataexcel = "data.ecxl"
-
+dataexcel = "data.xlsx"
+downloadthreads = 1000
 #
 def fetch_img(lat, long):
     response = requests.get(
@@ -44,17 +45,13 @@ os.makedirs("data",exist_ok=True)
 os.makedirs("data/true",exist_ok=True)
 os.makedirs("data/false",exist_ok=True)
 
-pool = ThreadPoolExecutor(max_workers=1000)
-file = open(dataexcel,"r")
-i = 0 
-while(true):
-    i = i+1
-    line = file.readline()
-    if(line == ""):
-        break
-    lat,log,imagetype = line.split(",")
+pool = ThreadPoolExecutor(max_workers=downloadthreads)
+
+wb = load_workbook(dataexcel,read_only = True)
+sheet = wb.active
+
+for i, row in enumerate(sheet.iter_rows(min_row=1, values_only=True), start=1):
+    lat,log,imagetype = row
     pool.submit(download_img,lat,log,"data/{}/img_{}.jpg".format(imgtype,i))
 
 pool.shutdown()
-file.close()
-print(fetch_img(40,73))
